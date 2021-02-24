0debcd788ce3008e827d2343656c8c4f28c28595
DhairyaPatel2006 <dhairyappatel23@gmail.com>
2021-02-24 18:35:14 +0530

    he

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var backImage,backgr;
 var player, player_running;
 var ground,ground_img;
-
+var bananaGroup, obstacleGroup;
 var END =0;
 var PLAY =1;
 var gameState = PLAY;
@@ -9,7 +9,8 @@ var gameState = PLAY;
 function preload(){
   backImage=loadImage("jungle.jpg");
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
-
+  banono = loadImage("banana.png");
+  pathar = loadImage("stone.png");
 }
 
 function setup() {
@@ -28,7 +29,9 @@ function setup() {
   ground = createSprite(400,350,800,10);
   ground.x=ground.width/2;
   ground.visible=false;
-  
+
+  bananaGroup = new Group();
+  obstacleGroup = new Group();
 }
 
 function draw() { 
@@ -46,8 +49,40 @@ function draw() {
     player.velocityY = player.velocityY + 0.8;
   
     player.collide(ground);
-
+    
+    bananas();
+    obstacles();
   }
 
   drawSprites();
+}
+
+function bananas(){
+  if (frameCount%80 === 0){
+    
+    banana = createSprite(620,120, 50, 50 )
+    banana.addImage(banono);
+    banana.scale = 0.1;
+    banana.y = random(120,200);
+    banana.velocityX = -4          
+    banana.lifetime = 220;
+    player.depth = banana.depth + 1
+    bananaGroup.add(banana);
+  }
+}
+
+function obstacles(){
+  if (frameCount%200 === 0){
+    
+    obstacle = createSprite(620,253,50,50);
+    obstacle.addImage(pathar);
+    obstacle.setCollider("circle", 0, 0, 180);
+    obstacle.scale = 0.13;
+    obstacle.velocityX = -4
+    obstacle.lifetime = 220;
+    obstacleGroup.add(obstacle);
+    
+  }
+  
+  
 }
